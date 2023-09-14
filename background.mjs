62ada8c8c62cbc5409d1ca6bc50f19698fa7ed1b
1inguini <9647142@gmail.com>
2023-09-14 22:34:08 +0900

    動くはず

--- background.mjs
+++ background.mjs
@@ -1,1 +1,58 @@
-browser.runtime.onMessage.addListener((scraped) => {});
+browser.runtime.onMessage.addListener(
+  async ({ year, month, day, hour, minute, is_private, link, note, tags }) => {
+    // [Nuita](https://nuita.net)
+    // get csrf-token
+    // await (
+    //   await fetch("https://nuita.net")
+    // ).body
+    //   .pipeThrough(new TextDecoderStream())
+    //   .pipeThrough(
+    //     new TextDecoderStream({
+    //       outside: true,
+    //       accm: "",
+    //       transform(text, controller) {
+    //         this.accm += text;
+
+    //         if (this.head) {
+    //         } else {
+    //         }
+    //         controller.enqueue(this.accm);
+    //         this.accm = "";
+    //       },
+    //     })
+    //   )
+    //   .pipeTo(
+    //     new WritableStream({
+    //       state: "",
+    //       write(text) {
+    //         this.state += text;
+    //         this.state.replace(/^.*<head>/, "<head>");
+    //       },
+    //     })
+    //   );
+
+    const authenticity_token = new DOMParser()
+      .parseFromString(
+        await (await fetch("https://nuita.net")).text(),
+        "text/html"
+      )
+      .head.getElementsByTagName("meta")["csrf-token"].content;
+    fetch("https://nuita.net/nweets", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        authenticity_token,
+        "nweet[statement]":
+          (link ? link + " " : "") +
+          (note ? note + " " : "") +
+          tags.map((tag) => "#" + tag).join(" "),
+        "nweet[privacy]": is_private ? "private" : "public",
+        "nweet[did_at]": `${year}-${month}-${day}T${hour}:${minute}:00+0900`,
+        commit: "ヌイート",
+      }).toString(),
+    });
+  }
+);
--- scrape.js
+++ scrape.js
@@ -1,5 +1,7 @@
 (async function () {
-  const { withId, withName } = new (await import("./util.mjs"))();
+  const { withId, withName } = await import(
+    browser.runtime.getURL("./util.mjs")
+  );
 
   [...document.forms].map((form) => {
     if (form.action === "https://shikorism.net/checkin") {
@@ -15,14 +17,14 @@
           day,
           hour,
           minute,
-          body: {
-            checked_in_at: `${year}-${month}-${day}T${hour}:${minute}:00+0900`,
-            tags: withName("tags").checked,
-            note: withName("note").checked,
-            is_private: withName("is_private").checked,
-            is_too_sensitive: withName("is_too_sensitive").checked,
-            discard_elapsed_time: withName("discard_elapsed_time").checked,
-          },
+          tags: withName("tags")
+            .value.split(/\p{White_Space}/u)
+            .filter((str) => str.length !== 0),
+          link: withName("link").value,
+          note: withName("note").value,
+          is_private: withName("is_private").checked,
+          is_too_sensitive: withName("is_too_sensitive").checked,
+          discard_elapsed_time: withName("discard_elapsed_time").checked,
         });
       });
     }
